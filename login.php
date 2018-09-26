af2ce5015f0cee1dd9d9d52143f326de8d430291
azzam <azzam.jiul@gmail.com>
2018-09-26 10:35:33 +0700

    first commit

---
+++ login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+</head>
+<body>
+	<form action="welcome.php" method="post">
+		<label>Username</label>
+		<input type="text" name="username" placeholder="type username here">
+		<label>Password</label>
+		<input type="password" name="password" placeholder="type username here">
+	</form>
+</body>
+</html>
